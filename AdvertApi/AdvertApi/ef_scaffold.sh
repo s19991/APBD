c0f15ed4079c1c47cc95d8dd0f2c34426ba50a2b
Fabian Schmidt <s19991@pjwstk.edu.pl>
2020-06-07 16:05:23 +0200

    fix: brakowalo tu -t by doprecyzowac te tabelki, ktore chce zamiast pobierac wszystkie

--- AdvertApi/AdvertApi/ef_scaffold.sh
+++ AdvertApi/AdvertApi/ef_scaffold.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 export PATH="$PATH:/home/fabians/.dotnet/tools"
 
-# dodaje tabelki za pomoca EF do Models/
+# Dodaje wybrane tabelki do Models za pomoca scaffold
 CONNECTION_STRING="\"Data Source=db-mssql.pjwstk.edu.pl; Initial Catalog=s19991; User Id=apbds19991; Password=admin\""
-dotnet ef dbcontext scaffold $CONNECTION_STRING Microsoft.EntityFrameworkCore.SqlServer -o Models/
+dotnet ef dbcontext scaffold $CONNECTION_STRING Microsoft.EntityFrameworkCore.SqlServer -o Models/ \
+-t Campaign \
+-t Banner \
+-t Building \
+-t Client 
