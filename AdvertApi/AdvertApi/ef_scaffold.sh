1617a072c221a9eb59acc2e664dee9821dbea10b
Fabian Schmidt <s19991@pjwstk.edu.pl>
2020-06-07 15:57:14 +0200

    fix: zmienna CONNECTION_STRING potrzebowala \"

--- AdvertApi/AdvertApi/ef_scaffold.sh
+++ AdvertApi/AdvertApi/ef_scaffold.sh
@@ -2,5 +2,5 @@
 export PATH="$PATH:/home/fabians/.dotnet/tools"
 
 # dodaje tabelki za pomoca EF do Models/
-CONNECTION_STRING="Data Source=db-mssql.pjwstk.edu.pl; Initial Catalog=s19991; User Id=apbds19991; Password=admin"
+CONNECTION_STRING="\"Data Source=db-mssql.pjwstk.edu.pl; Initial Catalog=s19991; User Id=apbds19991; Password=admin\""
 dotnet ef dbcontext scaffold $CONNECTION_STRING Microsoft.EntityFrameworkCore.SqlServer -o Models/
