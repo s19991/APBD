eed3c3fce4f8dbf638352f89c822deeb9a013766
Fabian Schmidt <s19991@pjwstk.edu.pl>
2020-06-08 19:44:38 +0200

    feat: wstepnie dodane metody do IDbService

--- AdvertApi/AdvertApi/DAL/IDbService.cs
+++ AdvertApi/AdvertApi/DAL/IDbService.cs
@@ -1,7 +1,15 @@
+using AdvertApi.DTO.Requests;
+
 namespace AdvertApi.DAL
 {
     public interface IDbService
     {
-        
+        public void RegisterUser(RegisterUserRequest request);
+
+        public void LoginUser(LoginUserRequest request);
+
+        public void GetCampaigns();
+
+        public void CreateCampaign(CreateCampaignRequest request);
     }
 }
--- AdvertApi/AdvertApi/DAL/MsSqlDbService.cs
+++ AdvertApi/AdvertApi/DAL/MsSqlDbService.cs
@@ -1,7 +1,27 @@
+using AdvertApi.DTO.Requests;
+
 namespace AdvertApi.DAL
 {
     public class MsSqlDbService : IDbService
     {
-        
+        public void RegisterUser(RegisterUserRequest request)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void LoginUser(LoginUserRequest request)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void GetCampaigns()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void CreateCampaign(CreateCampaignRequest request)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
